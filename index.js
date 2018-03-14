946b2fcd457b05420b6a5ee1b18eb0cd6054b346
Sonia  D <sduarte22@gmail.com>
2018-03-14 01:06:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+document.querySelector('');
+
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(target){
+return document.querySelector('div') ;
+}
+
+function increaseRankBy(n){
+      const rankedLists = document.getElementById('app').querySelectorAll('ul.ranked-list')
+      for(var i =0; i<rankedLists.length; i++){
+        rankedLists[i].innerHTML = parseInt(i+ n)
+      }
+}      
+
+function deepestChild(){
+  var results = document.getElementById('grand-node').querySelectorAll('div');
+   
+  
+    var values= Object.values(results)
+   return values.pop()
+    
+ 
+  
+}
