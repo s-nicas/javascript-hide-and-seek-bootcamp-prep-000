47f14a599eed7716ca75be29f271d0cf214d1f03
Sonia  D <sduarte22@gmail.com>
2018-03-14 00:16:52 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+document.querySelector('');
+
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(target){
+return document.querySelector('div') ;
+}
+
+function increaseRankBy(n){
+      const rankedLists = document.getElementById('app').querySelectorAll('ul.ranked-list')
+      for(var i =0; i<rankedLists.length; i++){
+        rankedLists[i].innerHTML = parseInt(i+ n)
+      }
+}      
+
+function deepestChild(){
+  
+  
+  
+}
